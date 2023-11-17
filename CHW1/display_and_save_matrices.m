e625040d8286f837a2585082c97da98fe9111788
Theborna <borna710@gmail.com>
2023-11-17 14:11:01 +0330

    added report for CHW1

--- CHW1/display_and_save_matrices.m
+++ CHW1/display_and_save_matrices.m
@@ -8,15 +8,19 @@ function display_and_save_matrices(graph, filename)
         disp('Weight Matrix for Graph');
         disp(graph.W);
     end
-    
+
     % Save matrices to a text file
     fid = fopen(filename, 'w');
     fprintf(fid, 'Adjacency Matrix for Graph:\n');
     fprintf(fid, '%g ', full(graph.A));
+    fprintf(fid, '\n\nLatex:\n');
+    fprintf(fid, '%s ', latex(full(graph.A) + 0 * sym('x')));
     
     if isfield(graph, 'W')
         fprintf(fid, '\n\nWeight Matrix for Graph:\n');
         fprintf(fid, '%g ', full(graph.W));
+        fprintf(fid, '\n\nLatex:\n');
+        fprintf(fid, '%s ', latex(full(graph.W) + 0 * sym('x')));
     end
     
     fclose(fid);
